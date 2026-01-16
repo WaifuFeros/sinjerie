8e96ecc443d78525a4a4fc6f4b2bad9facbf32b8
WaifuFeros <payet.pep@gmail.com>
2026-01-16 14:49:54 +0100

    # add starting item

--- Assets/Features/Drag&Drop/Scripts/DropItemZoneController.cs
+++ Assets/Features/Drag&Drop/Scripts/DropItemZoneController.cs
@@ -1,4 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
 public class DropItemZoneController : GameDroppableZoneController<DraggableItem>
 {
+    [SerializeField]
+    private DraggableItem startingItem;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (startingItem == null)
+            return;
+
+        tryAddDraggable(startingItem);
+    }
 }
