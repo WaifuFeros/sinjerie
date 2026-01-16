0c18949d253faf69329322b9e3e456e55c86bc22
Yvann Nadal <72645202+Yvann-Nadal@users.noreply.github.com>
2026-01-16 12:04:16 +0100

    add debug GPS

--- Assets/Script/BDJ/GPSManager.cs
+++ Assets/Script/BDJ/GPSManager.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class GPSManager : MonoBehaviour
 {
+    public TextMeshProUGUI positionText;
+
     public float latitude;
     public float longitude;
 
     IEnumerator Start()
     {
 #if UNITY_ANDROID
-        if (!UnityEngine.Android.Permission.HasUserAuthorizedPermission(UnityEngine.Android.Permission.FineLocation))
+        if (!UnityEngine.Android.Permission.HasUserAuthorizedPermission(
+            UnityEngine.Android.Permission.FineLocation))
         {
-            UnityEngine.Android.Permission.RequestUserPermission(UnityEngine.Android.Permission.FineLocation);
+            UnityEngine.Android.Permission.RequestUserPermission(
+                UnityEngine.Android.Permission.FineLocation);
         }
 #endif
 
         if (!Input.location.isEnabledByUser)
         {
-            Debug.Log("GPS désactivé par l'utilisateur");
+            Debug.Log("GPS désactivé");
+            if (positionText != null)
+                positionText.text = "GPS désactivé";
             yield break;
         }
 
@@ -26,6 +33,8 @@ public class GPSManager : MonoBehaviour
         int maxWait = 20;
         while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
         {
+            if (positionText != null)
+                positionText.text = "Recherche GPS...";
             yield return new WaitForSeconds(1);
             maxWait--;
         }
@@ -33,6 +42,8 @@ public class GPSManager : MonoBehaviour
         if (Input.location.status != LocationServiceStatus.Running)
         {
             Debug.Log("GPS indisponible");
+            if (positionText != null)
+                positionText.text = "GPS indisponible";
             yield break;
         }
 
@@ -40,6 +51,12 @@ public class GPSManager : MonoBehaviour
         longitude = Input.location.lastData.longitude;
 
         Debug.Log($"GPS OK : {latitude} / {longitude}");
+
+        if (positionText != null)
+        {
+            positionText.text =
+                $"Latitude : {latitude:F5}\nLongitude : {longitude:F5}";
+        }
 
         Input.location.Stop();
     }
