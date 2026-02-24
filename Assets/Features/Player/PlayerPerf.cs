594dd9cbc37dae76e8192b046a1b8e9cc77baf50
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2026-02-24 15:16:49 +0100

    + scene menu and script menu created

---
+++ Assets/Features/Player/PlayerPerf.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPerf : MonoBehaviour
+{
+    private int BestScore = 0;
+    private int MaxRoom = 0;
+}
