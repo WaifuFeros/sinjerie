577fc38e293b09a8327f099be45d54f9ad16906c
Okiosk <victorouzaud@gmail.com>
2026-06-16 18:38:29 +0200

    + ajout behevior venere

---
+++ Assets/Features/IAenemy/EnemyBehaviorDataSO/Venere.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/Behavior/Vénère")]
+public class Venere : EnemyBehaviorSO
+{
+    public override ObjetSO[] ChooseItem(ObjetSO[] objs, int health, int stamina)
+    {
+        List<ObjetSO> chosenItems = new List<ObjetSO>();
+        int staminaAvailable = stamina;
+
+        foreach (var obj in objs)
+        {
+            ObjetSO selecObj = objs[Random.Range(0, objs.Length)];
+            if (staminaAvailable - selecObj.objetWeight >= 0 && selecObj.objectType == ObjetEffectType.Attack)
+            {
+                chosenItems.Add(selecObj);
+                staminaAvailable -= selecObj.objetWeight;
+            }
+        }
+        return chosenItems.ToArray();
+    }
+}
