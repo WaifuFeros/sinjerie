f7c9425a8cb24decb0d91fc308e4e7b672ee2717
Okiosk <victorouzaud@gmail.com>
2026-06-16 18:51:56 +0200

    + ajout behevior peureux

--- Assets/Features/IAenemy/EnemyBehaviorDataSO/Peureux.cs
+++ Assets/Features/IAenemy/EnemyBehaviorDataSO/Peureux.cs
@@ -1,18 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vener : MonoBehaviour
+[CreateAssetMenu(menuName = "Enemy/Behavior/Peureux")]
+public class Peureux : EnemyBehaviorSO
 {
-    // Start is called before the first frame update
-    void Start()
+    public override ObjetSO[] ChooseItem(ObjetSO[] objs, int health, int stamina)
     {
-        
-    }
+        List<ObjetSO> chosenItems = new List<ObjetSO>();
+        int staminaAvailable = stamina;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // tri item de heal et atk
+        List<ObjetSO> healItems = new List<ObjetSO>();
+        List<ObjetSO> atkItems = new List<ObjetSO>();
+        foreach (var obj in objs)
+        {
+            switch (obj.objectType)
+            {
+                case ObjetEffectType.Heal:
+                    healItems.Add(obj);
+                    break;
+                case ObjetEffectType.Attack:
+                    atkItems.Add(obj);
+                    break;
+            }
+        }
+
+
+        foreach (var obj in objs)
+        {
+            ObjetSO selecObj;
+            if (Random.Range(0, 3)== 0)
+                selecObj = atkItems[Random.Range(0, atkItems.Count)];
+            else
+                selecObj = healItems[Random.Range(0, healItems.Count)];
+
+            if (staminaAvailable - selecObj.objetWeight >= 0)
+            {
+                chosenItems.Add(selecObj);
+                staminaAvailable -= selecObj.objetWeight;
+            }
+        }
+        return chosenItems.ToArray();
     }
 }
