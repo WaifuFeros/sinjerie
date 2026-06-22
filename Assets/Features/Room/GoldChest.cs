31c5d07c9a7ed13697d4029f50143419dbd585af
Okiosk <victorouzaud@gmail.com>
2026-06-22 05:34:32 +0200

    + ajout asset salle trésor

---
+++ Assets/Features/Room/GoldChest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GoldChest : MonoBehaviour
+{
+    [Header("Gold")]
+    [SerializeField] private int goldGiven = 20;
+    [Header("Anim")]
+    [SerializeField] private float bounceDuration = 0.6f;
+    [SerializeField] private float bounceStrength = 20f;
+    [SerializeField] private float clickScaleMultiplier = 1.3f;
+    [SerializeField] private float clickDuration = 0.2f;
+
+    void Start()
+    {
+        transform.DOLocalMoveY(transform.localPosition.y + bounceStrength, bounceDuration)
+            .SetEase(Ease.InOutQuad)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void OnChestClicked()
+    {
+        PlayerManager.Instance.AddGold(goldGiven);
+        transform.DOKill();
+        transform.DOScale(transform.localScale * clickScaleMultiplier, clickDuration / 2)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                transform.DOScale(Vector3.zero, clickDuration / 2)
+                    .SetEase(Ease.InQuad)
+                    .OnComplete(() => Destroy(gameObject));
+            });
+    }
+}
