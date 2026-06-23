e9bae01c055053e366db1afe8aafa9b1d6f0c0ca
Okiosk <victorouzaud@gmail.com>
2026-06-23 15:22:05 +0200

    + fix main menu

--- Assets/Features/MainMenu/Menu.cs
+++ Assets/Features/MainMenu/Menu.cs
@@ -33,8 +33,6 @@ public class Menu : MonoBehaviour
     {
         TransitionManager.Instance.TransitionWithAction(() => {
             SceneLoadManager.Instance.LoadSceneAsActive(_gameSceneName);
-            //SceneManager.sceneLoaded += OnGameSceneLoaded;
-            //SceneManager.LoadScene(_gameSceneName, LoadSceneMode.Additive);
 
         });
     }
@@ -48,46 +46,24 @@ public class Menu : MonoBehaviour
         });
     }
 
-    //private void OnGameSceneLoaded(Scene scene, LoadSceneMode mode)
-    //{
-    //    if (scene.name == _gameSceneName)
-    //    {
-    //        SceneManager.SetActiveScene(scene);
-    //        SceneManager.sceneLoaded -= OnGameSceneLoaded;
-    //        SceneManager.UnloadSceneAsync("HomeMenu");
-    //    }
-    //}
-
     public void OpenEncyclopedia()
     {
         _EncyclopediaPanel.SetActive(true);
-        _ParametresButton.gameObject.SetActive(false);
-        _StartButton.gameObject.SetActive(false);
-        _EncyclopediaButton.gameObject.SetActive(false);
     }
 
     public void CloseEncyclopedia()
     {
         _EncyclopediaPanel.SetActive(false);
-        _ParametresButton.gameObject.SetActive(true);
-        _StartButton.gameObject.SetActive(true);
-        _EncyclopediaButton.gameObject.SetActive(true);
 
     }
 
     public void OpenParametres()
     {
         _ParametresPanel.SetActive(true);
-        _ParametresButton.gameObject.SetActive(false);
-        _StartButton.gameObject.SetActive(false);
-        _EncyclopediaButton.gameObject.SetActive(false);
     }
 
     public void CloseParametres()
     {
         _ParametresPanel.SetActive(false);
-        _ParametresButton.gameObject.SetActive(true);
-        _StartButton.gameObject.SetActive(true);
-        _EncyclopediaButton.gameObject.SetActive(true);
     }
 }
